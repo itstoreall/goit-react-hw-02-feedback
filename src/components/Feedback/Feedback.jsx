9bec9a7a7842d0d0ce88f853211480f8b30c5295
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-20 18:01:52 +0200

    adds two methods: countTotalFeedback and countPositiveFeedbackPercentage

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import style from './Profile.module.scss';
+import Controls from './Controls';
+import Values from './Values';
 import style from './Feedback.module.scss';
 
 class Feedback extends Component {
@@ -11,10 +12,9 @@ class Feedback extends Component {
     bad: 0
   };
 
-  
   // static propTypes = {};
 
-  handleIncrement = e => {
+  handleGood = e => {
     // Не основываясь на предидущем
     // this.setState({good: 50})
     this.setState((prevState) => {
@@ -22,49 +22,57 @@ class Feedback extends Component {
         good: prevState.good + 1
       }
     })
-  };
+  }
 
   handleNeutral = e => {
-    this.setState((prevState) => ({
+    this.setState((prevState) => {
+      return {
         neutral: prevState.neutral + 1
-      }))
-  };
+      }
+    })
+  }
 
   handleBad = e => {
     this.setState((prevState) => {
       return {
         bad: prevState.bad + 1
-      }
-    })
+      }})
+  }
+
+  countTotalFeedback = () => {
+    return this.state.good
+      + this.state.neutral
+      + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(this.state.good
+      * 100
+      / (this.state.good
+      + this.state.neutral 
+      + this.state.bad
+      ))
   }
 
   render() {
     return (
       <div className={style.Feedback}>
         <p>Please leave feedback</p>
-        <div className={style.buttons}>
-          <button 
-            type='button' 
-            onClick={this.handleIncrement}>
-                Good
-          </button>
-          <button 
-            type='button' 
-            onClick={this.handleNeutral}>
-                Neutral
-          </button>
-          <button 
-            type='button' 
-            onClick={this.handleBad}>
-                Bad
-          </button>
-        </div>
+        <Controls
+          onGood={this.handleGood}
+          onNeutral={this.handleNeutral}
+          onBad={this.handleBad}
+          style={style}
+        />
         <p>Statistics</p>
-        <ul className={style.indicators}>
-          <li>Good: <span>{this.state.good}</span></li>
-          <li>Neutral: <span>{this.state.neutral}</span></li>
-          <li>Bad: <span>{this.state.bad}</span></li>
-        </ul>
+        <Values
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.countTotalFeedback()}
+          positiveFeedback={this.countPositiveFeedbackPercentage()}
+          style={style}
+        />
       </div>
     );
   };
